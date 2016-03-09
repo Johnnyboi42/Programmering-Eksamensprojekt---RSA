fef9b32bb2f6e5ddc7977108daebe17087586140
Johnnyboi42 <johsgg@gmail.com>
2016-03-09 11:33:25 +0100

    Saved primes from 2 to 2**16 Successfully reads file to see if number already exists in prime list

--- saving_prime_ryan.py
+++ saving_prime_ryan.py
@@ -1,13 +1,26 @@
-readf = open('primtal.txt')
-appf = open('primtal.txt','a')
+readfile = open('primtal.txt')
 
-def save_primes(lim):
+def in_file(numberino): # Decides if the number is in the file 'primtal.txt'
+    readf = open('primtal.txt')
+    readf.seek(0)
+    for line in readf:
+        if int(line) == numberino:
+            return True
+
+
+def save_primes(lim): # Saves primes in file 'primtal.txt' if number is prime and not in file already
     for number in range(2,lim):
-        if number not in readf:
-            for line in readf:
+        if in_file(number) != True:
+            is_prime = True
+            readfile.seek(0)
+            for line in readfile:
                 if number % int(line) == 0:
-                    appf.write(str(number) + '\n')
+                    is_prime = False
                     break
+            if is_prime:
+                appendf = open('primtal.txt','a')
+                appendf.write(str(number) + '\n')
 
-
-save_primes(20)
+save_primes(2**16)
+readfile.seek(0)
+readfile.read()
