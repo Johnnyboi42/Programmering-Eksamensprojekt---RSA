90def9591c50ed9b285cd96b28cf8e596169ffe3
Johnnyboi42 <johsgg@gmail.com>
2016-04-09 10:24:19 +0200

    Changed save_primes to start from the biggest exponent currently in primtal.txt, which is a number stored seperately in biggestexponent.txt.
    Added public_key to primtal.py to calculate the public key (n,e).
    
    Changed find_primes2() to return 2 random primes from a bit size with a given range.

--- primtal.py
+++ primtal.py
@@ -22,10 +22,25 @@ def find_primes(bits, buffer): #Finds primes within a given range of bits
         arr_primes.remove(0)
     print(arr_primes)
 
-def find_primes(bits,range):
+def find_primes2(bits,range): # Finds two primes within a given range of bits and returns them
     prime_list = primes_in_range(bits)
     if prime_list.len() > 5:
-
-        random.randint(0,prime_list.len()-1)
+        rInt = random.randint(0,prime_list.len()-1)
+        firstPrime = prime_list[rInt]
+        prime_list.remove(prime_list[rInt])
+        rInt = random.randint(0,prime_list.len()-1)
+        secondPrime = prime_list[rInt]
+        return (firstPrime, secondPrime)
     else:
         save_primes(2**(bits+1))
+        find_primes2(bits,range)
+
+def public_key(bits, range): # Returns a public key (n,e) for RSA encryption
+    # DO THINGS
+    firstPrime, secondPrime = find_primes2(bits, range)
+    n = firstPrime * secondPrime
+    totient = (firstPrime-1) * (secondPrime-1)
+    decoy, e = find_primes2(bits/2, range)
+    return (n,e)
+def private_key(n,e):
+    # DO THINGS, BUT PRIVATELY
